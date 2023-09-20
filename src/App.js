e4fb49be56fa7a8cc852673f2e98336b775858a1
j-goodman <johngoodman@pursuit.org>
2023-09-20 14:38:22 -0400

    Add title.

--- src/App.js
+++ src/App.js
@@ -8,18 +8,21 @@ function App() {
     `function addTwoNumbers(a, b) {\n  return a + b;\n}`
   );
   return (
-    <CodeEditor
-      value={code}
-      language="js"
-      placeholder="Please enter JS code."
-      onChange={(evn) => setCode(evn.target.value)}
-      padding={15}
-      style={{
-        fontSize: 12,
-        backgroundColor: "#f5f5f5",
-        fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-      }}
-    />
+    <section>
+      <h2>stopwatch</h2>
+      <CodeEditor
+        value={code}
+        language="js"
+        placeholder="Please enter JS code."
+        onChange={(evn) => setCode(evn.target.value)}
+        padding={15}
+        style={{
+          fontSize: 12,
+          backgroundColor: "#f5f5f5",
+          fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+        }}
+      />
+    </section>
   );
 }
 
